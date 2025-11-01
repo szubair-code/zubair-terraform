abf0bc0fc78d3d3055a34a10bdd9eb95265d3399
zubair <shaikzubair3492@gmail.com>
2025-11-01 20:38:57 +0530

    vnet added

---
+++ vnet.tf
@@ -0,0 +1,7 @@
+resource "azurerm_virtual_network" "vnet" {
+    name = var.vnet-name
+    address_space = var.ipaddress
+    resource_group_name = azurerm_resource_group.AP1.name
+    location = azurerm_resource_group.AP1.location
+
+}
